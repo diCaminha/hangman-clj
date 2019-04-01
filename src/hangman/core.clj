db6047fdbe15001a71d21fae1eda025060288425
Denis Neres Caminha <denis.caminha-for@MVFOR.local>
2019-04-01 13:03:58 -0300

    add messages final game

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -1,12 +1,14 @@
 (ns hangman.core
   (:gen-class))
 
- 
+(defn loose-msg [] (print "you loose"))
+(defn win-msg [] (print "you win"))
+
 (def total-lifes 5)
 
-(defn game [lifes]
+(defn game [lifes words right-letters]
 	(if (= lifes 0)
-		(do
+		(loose-message)
 		(game (dec lifes)))
   
   
